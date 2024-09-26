ad2d2b7205be225865ffab095e5b6165b9968e41
Tim Marwick <tim.marwick@pruple.telstra.com>
2024-09-26 15:11:46 +1000

    Format all, and lesson 1 from NextJS tutorial
    https://nextjs.org/learn/dashboard-app/css-styling

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,4 @@
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,7 +15,7 @@ const links = [
 export default function NavLinks() {
   return (
     <>
-      {links.map((link) => {
+      {links.map(link => {
         const LinkIcon = link.icon;
         return (
           <a
